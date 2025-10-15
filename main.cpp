0e33ba326cf585d75a010ee309b914d05feaead9
Dmitry <halfybro@gmail.com>
2025-10-15 23:27:34 +0300

    (fix): Изменён запуск калькулятор, добавлен файл конфигурации

--- main.cpp
+++ main.cpp
@@ -1,19 +1,8 @@
-#include <iostream>
-#include <string>
 #include "Calculator.h"
-
 
 using namespace std;
 int main(){
-    string expression;
-    getline(cin, expression);
-
-    Calculator calc(expression);
-
-    calc.evaluate(expression);
-
-    cout << "Результат " << calc.get_result() << endl;
-
-
+    Calculator calc;
+    calc.start();
     return 0;
 }
